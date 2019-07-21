3b87392594145042cd4f4f3283addae633d1a862
Hradec <hradec@hradec.com>
2019-07-20 20:02:12 -0700

    added a build of the image, gathering the version from Docekrfile. Added extra code to gather the connected arduino ports and passthrough the docker automatically.

--- arduino.sh
+++ arduino.sh
@@ -2,6 +2,20 @@
 # Starts the Arduino IDE using the docker image.
 # see also: ...
 
+# extract version from Dockerfile
+version=$(grep 'ENV ARDUINO_IDE_VERSION' Dockerfile | awk '{print $(NF)}')
+docker build . -t tombenke/darduino:v$version
+
+
+# add current available arduino TTY's
+extra=""
+for n in $(ls /dev/ttyACM*) ; do
+	extra="-v $n:$n $extra"
+done
+for n in $(ls /dev/ttyUSB*) ; do
+	extra="-v $n:$n $extra"
+done
+
 #    -v /dev/ttyACM0:/dev/ttyACM0 \
 #    -v /dev/ttyUSB0:/dev/ttyUSB0 \
 docker run \
@@ -15,6 +29,7 @@ docker run \
     -v $HOME/topics:/topics \
     -v $HOME/topics/arduino:/home/developer/Arduino \
     --name arduino \
-    tombenke/darduino:v1.8.5 \
+    $extra \
+    tombenke/darduino:v$version \
     arduino
 
